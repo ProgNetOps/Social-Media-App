f02207f7ede1a00951d94d1ada8ad6ecd10e4dab
Suberu 'Seyi <suberuseyi@gmail.com>
2025-05-12 22:58:10 +0100

    DbContext created and registered in the container

--- SocialMediaApp/Program.cs
+++ SocialMediaApp/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SocialMediaApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Database configuration
+string? dbConnectionString = builder.Configuration.GetConnectionString("Default");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(dbConnectionString));
+
 
 var app = builder.Build();
 
--- SocialMediaApp/obj/Debug/net9.0/SocialMediaApp.AssemblyInfo.cs
+++ SocialMediaApp/obj/Debug/net9.0/SocialMediaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialMediaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b67207e8db6514ebea8f8ce1cb097ce74d280319")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialMediaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialMediaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
